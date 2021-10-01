830a6f104959c4ac9e09be61bf8482cb21a87807
mianjoto <78712025+mianjoto@users.noreply.github.com>
2021-10-01 15:17:16 -0400

    re-did ErrP averaging and plotting

--- main.py
+++ main.py
@@ -14,14 +14,38 @@ from sklearn.svm import SVC
 
 
 def main():
-    total_data, errp_data, labels, channels = load_eeg_pkl()
-    # plot_eeg(errp_data, channels, 'ErrP', 'Amplitude')
-    find_best_accuracy(0.87, errp_data, channels, labels, 1,
-                       train_statically=True)
-    plot_eeg(total_data, channels, 'epochs', 'amplitude')
+    ern_avg, non_ern_avg, labels, channels = load_eeg_pkl()
+
+    # Plot the data
+    print('Preparing the EEG data to plot...')
+    title = 'ErrP Amplitude Average over 200 Samples'
+    xlabel = 'ErrP Data'
+    ylabel = 'Amplitude'
+
+    # Assign a DataFrame to the passed datas
+    X1 = pd.DataFrame(ern_avg.T, columns=channels)
+    X2 = pd.DataFrame(non_ern_avg.T, columns=channels)
+
+    print('Plotting EEG data...')
+    # ErrP data
+    plt.figure(1)
+    sns.lineplot(data=X1, dashes=False)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+
+    # Non-ErrP data
+    plt.figure(2)
+    sns.lineplot(data=X2, dashes=False)
+    plt.title(f'Non-{title}')
+    plt.xlabel(f'Non-{xlabel}')
+    plt.ylabel(ylabel)
+
+    print('Displayed EEG data')
+    plt.show()
 
 
-def load_eeg_pkl(filepath: str = None, load_all: bool = False):
+def load_eeg_pkl(filepath: str = None, return_all: bool = False):
     """Load EEG data from pkl file
 
     Loads modified EEG data provided by Ben Poole. The PKL must contain
@@ -29,39 +53,6 @@ def load_eeg_pkl(filepath: str = None, load_all: bool = False):
     `dataset`, `subject`, `trial`, and `channels` for this method to work.
 
     The PKL file is a nested dictionary of dictionaries.
-
-    Parameters
-    ----------
-    filepath: str, default=None
-        filepath for the .pkl file. if not specified, find the file
-        ``BGSInt-1-ar.pkl`` in the current working directory
-
-    load_all: bool, default=False
-        Whether to return all  dictionaries and tuples
-
-    Returns
-    -------
-    (epoch_mean_list, channels) : tuple if ``load_all`` is False
-        Tuple of data, with the following attributes:
-
-        epoch_mean_list: list
-            The list of epochs that are averaged
-        channels: list if ``load_all`` is True
-            The list that contains the names of the channels
-
-    dataset: ndarray if ``load_all`` is True
-        The data that was loaded from the PKL file. Shape varies on the file
-    trial_tags: set if ``load_all`` is True
-        The set that contains trial tags
-    trial_indexes:  dict if ``load_all`` is True
-        The dict that contains trial indexes, essentially the same as
-        ``trial_tags``
-    dataset_tags: set if ``load_all`` is True
-        The set that describes the dataset
-    subject_tags: set if ``load_all`` is True
-        The set that describes the subject identification
-    channels: list if ``load_all`` is True
-        The list that contains the names of the channels
     """
     if filepath is None:
         d = os.getcwd()
@@ -81,44 +72,23 @@ def load_eeg_pkl(filepath: str = None, load_all: bool = False):
         trial_tags = tags.get('trial')
         channels = file_dataset.get('channels')
 
-    # Get an array of the indexes of the epochs that have an associated ErrP value
+    # Get ndarray of indexes of epochs that have associated ERN value
     ern_occurrences: np.ndarray = np.where(labels == 1)[0]
+    non_ern_occurrences: np.ndarray = np.where(labels == 0)[0]
 
-    # Master list that will hold the list of mean values
-    epoch_mean_all_channels = []
+    # Get array of the epochs that DO and DO NOT have an associated ErrP value
+    ern_epochs = dataset[ern_occurrences]
+    non_ern_epochs = dataset[non_ern_occurrences]
 
-    # Store mean of each column into a new ndarray
-    total_rows = np.shape(dataset)[0]
-    print(f'{total_rows=}')
-    total_channels = len(channels)
+    # Get average across all samples that DO and DO NOT have associated ERN
+    ern_epochs_avg = np.mean(ern_epochs, axis=0)
+    non_ern_epochs_avg = np.mean(non_ern_epochs, axis=0)
 
-    for channel in np.arange(total_channels):
-        # Initialize ndarray to store mean vals for each channel
-        mean_ndarray = np.empty(total_rows, dtype=float64)
-
-        # Get mean of each column and store it
-        for epoch_row in np.arange(total_rows):
-            mean = np.mean(dataset[epoch_row, channel, :])
-            mean_ndarray[epoch_row] = mean
-
-        # Add ndarray to master list
-        epoch_mean_all_channels.append(mean_ndarray)
-
-    # List that stores the epoch sets, which is the epoch (1 sec) and the number
-    # of samples per epoch (200 samples)
-    epoch_all_channels = []
-
-    # Fill the list with an epoch set for each channel
-    for i in np.arange(np.size(dataset, axis=1)):
-        epoch_all_channels.append(dataset[:, i, :])
-
-    print('Loaded EEG data')
-
-    if load_all is True:
-        return dataset, epoch_all_channels, epoch_mean_all_channels, labels, \
-               event_ids, trial_tags, trial_indexes, dataset_tags, \
-               subject_tags, channels
-    return epoch_all_channels, epoch_mean_all_channels, labels, channels
+    if return_all:
+        return dataset, ern_epochs, ern_epochs_avg, non_ern_epochs, \
+               non_ern_epochs_avg, labels, event_ids, trial_tags, trial_indexes, \
+               dataset_tags, subject_tags, channels
+    return ern_epochs_avg, non_ern_epochs_avg, labels, channels
 
 
 def find_best_accuracy(target_acc: float, epoch_all_channels, channels,
@@ -161,7 +131,7 @@ def find_best_accuracy(target_acc: float, epoch_all_channels, channels,
 
 
 def train_svm(train_statically: bool, epoch_set, labels, kernel: str,
-                     C_value: int, test_size: float = None):
+              C_value: int, test_size: float = None):
     """Statically train the SVM classifier by using the same data each time.
     Should yield the same optimal C_value every single time
     """
@@ -188,26 +158,6 @@ def train_svm(train_statically: bool, epoch_set, labels, kernel: str,
     return accuracy, y_pred
 
 
-def plot_eeg(epoch_mean_list: list, channels, xlabel: str, ylabel: str):
-    print('Preparing the EEG data to plot...')
-
-    # Make empty ndarray
-    epoch_shape = np.shape(epoch_mean_list[1])
-    data = np.empty((epoch_shape[0], 4), dtype=float64)
-
-    # Compile the list to one ndarray
-    for index, epoch_set in enumerate(epoch_mean_list):
-        data[:, index] = epoch_set[:, index]
-
-    X = pd.DataFrame(data, columns=channels)
-    print(X)
-
-    print('Plotting EEG data...')
-    sns.lineplot(data=X, dashes=False)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    print('Displayed EEG data')
-    plt.show()
 
 
 if __name__ == "__main__":
