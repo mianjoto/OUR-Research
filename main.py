0afca1acb2af2b7cbed464262c0827c60bd8a58c
mianjoto <78712025+mianjoto@users.noreply.github.com>
2021-10-08 02:33:05 -0400

    optimization of roc values

--- main.py
+++ main.py
@@ -1,61 +1,37 @@
 import os
 import pickle
 from os.path import join
+from typing import Tuple
 
 import numpy as np
 import pandas as pd
 import seaborn as sns
 from matplotlib import pyplot as plt
 from numpy import int16
-from sklearn import svm, metrics
-from sklearn.metrics import roc_curve, auc, accuracy_score, RocCurveDisplay
+from sklearn import svm
+from sklearn.metrics import roc_curve, auc
 from sklearn.model_selection import StratifiedKFold
 
 
 def main():
     ern_avg, non_ern_avg, dataset, labels, event_ids, channels = load_eeg_pkl()
 
-    chnl, chnl_idx = channels[-1], (len(channels) - 1)  # The channel we will
-    # observe
+    # The channel we will observe is Cn
+    chnl, chnl_idx = channels[-1], (len(channels) - 1)
 
     # Plot the data
-    print('Preparing the EEG data to plot...')
-    title = 'ErrP Amplitude Average over 200 Samples'
-    xlabel = 'ErrP Data'
-    ylabel = 'Amplitude'
+    # errp_labels = ('ErrP Amplitude Average over 200 Samples', 'ErrP Data',
+    #                'Amplitude')
+    # non_errp_labels = ('Non-ErrP Amplitude Average over 200 Samples',
+    #                    'Non-ErrP Data', 'Amplitude')
+    # plot_eeg(ern_avg, errp_labels, fig_num=1, columns=channels, show=True)
+    # plot_eeg(ern_avg, non_errp_labels, fig_num=2, columns=channels, show=True)
 
-    # Assign a DataFrame to the passed datas
-    X1 = pd.DataFrame(ern_avg.T, columns=channels)
-    X2 = pd.DataFrame(non_ern_avg.T, columns=channels)
-    X3 = pd.DataFrame(dataset[:, chnl_idx, :])
-
-    # ErrP data
-    plt.figure(1)
-    sns.lineplot(data=X1, dashes=False)
-    plt.title(title)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-
-    # Non-ErrP data
-    plt.figure(2)
-    sns.lineplot(data=X2, dashes=False)
-    plt.title(f'Non-{title}')
-    plt.xlabel(f'Non-{xlabel}')
-    plt.ylabel(ylabel)
-
-    # All data
-    plt.figure(3)
-    sns.lineplot(data=X3, dashes=False)
-    plt.title(f'Total {title}')
-    plt.xlabel(f'All {xlabel}')
-    plt.ylabel(ylabel)
-
-    # plt.show()
+    X = pd.DataFrame(dataset[:, chnl_idx, :])
 
     # Initialize the classifier
-    n_splits = 5
+    skf_bounds = (2, 200)
     clf = svm.SVC(kernel='rbf', C=5)
-    skf = StratifiedKFold(n_splits=n_splits)
     y = pd.DataFrame(labels)  # 1D nparray of 1s and 0s to indicate ErrPs
 
     # # Make a new matplotlib figure and decorate it
@@ -69,81 +45,29 @@ def main():
     plt.xlim([0, 1])
     plt.ylim([0, 1])
 
-    print(f'Training and splitting data...')
-    X_train_lst = []  # Not used
-    y_train_lst = []  # Not used
+    clf, svm_roc, logistic_roc = best_n_splits(clf, skf_bounds, X, y)
+    fpr, tpr, log_auc = logistic_roc
+    svm_fpr, svm_tpr, svm_auc = svm_roc
 
-    for i, (train_index, test_index) in enumerate(skf.split(X3, y)):
-        X_train, X_test = np.array(X3.iloc[train_index]), \
-                          np.array(X3.iloc[test_index])
-        y_train, y_test = np.array(y.iloc[train_index]).ravel(), \
-                          np.array(y.iloc[test_index]).ravel()
-
-        # Get positive and negative occurrences
-        pos = np.where(y_train == 1)[0]
-        neg = np.where(y_train == 0)[0]
-        # print(f'{i=}, pos occurrences = {pos.shape[0]} ---- neg occurrences ='
-        #       f' {neg.shape[0]}')
-
-        # Get X values that have ERN and do not have ERN
-        Xpos = X_train[pos, :]
-        Xneg = X_train[neg, :]
-
-        np.vstack((Xpos, Xneg))  # Stack all positive X's on the negative X's
-
-        ds_idx = Xneg.shape[0]  # Get index where we will begin downsampling
-
-        # Sample Xneg array len(XPos) many times
-        rand_ds_idx = np.random.randint(0, ds_idx, len(Xpos))
-
-        # Copy the array with the equal pos and equal downsampled neg values
-        X_train1 = np.vstack((Xpos, Xneg[rand_ds_idx]))
-
-        ypos = np.ones(Xpos.shape[0])
-        yneg = np.zeros(Xpos.shape[0])
-
-        y_train1 = np.concatenate((ypos, yneg), axis=0)
-
-        # Apply weights of positive : neg event to the classifier
-        pos_weight = Xpos.shape[0]
-        neg_weight = Xneg.shape[0]
-        clf.class_weight = {0: neg_weight, 1: pos_weight}
-
-        # Fit the data to the classifier
-        clf.fit(X_train1, y_train1)
-
-        # Save the values used to train the classifier
-        X_train_lst.append(X_train1)
-        y_train_lst.append(y_train1)
-
-    # Predict using the entire dataset
-    y_pred = clf.predict(X3)
-    fpr, tpr, threshold = roc_curve(y, y_pred)
-    area_under_curve = auc(fpr, tpr)
-
-    y_pred_svm = clf.decision_function(X3)
-    svm_fpr, svm_tpr, threshold = roc_curve(y, y_pred_svm)
-    svm_area_under_curve = auc(svm_fpr, svm_tpr)
+    # # Train and split the data
+    # X_train_lst, X_test, y_train_lst, y_test = skf_split_train(n_splits, X3, y)
+    # clf = svm_fit_clf(clf, (X_train_lst, y_train_lst))
+    #
+    # # Get ROC using the entire dataset
+    # logistic_roc, svm_roc = get_roc_data(clf, X3, y)
+    # fpr, tpr, area_under_curve = logistic_roc
+    # svm_fpr, svm_tpr, svm_area_under_curve = svm_roc
 
     # Plot ROC data
     plt.plot(svm_fpr, svm_tpr, linestyle='-',
-             label=f'{chnl} (svm_auc = %.3f)' % svm_area_under_curve)
+             label=f'{chnl} (svm_auc = %.3f)' % svm_auc)
     plt.plot(fpr, tpr, marker='.', label=f'{chnl} (auc = %.3f)' %
-                                         area_under_curve)
+                                         log_auc)
 
     plt.legend(loc='lower right')
 
     plt.show()
 
-
- #y_pred = clf.predict(X_test)
-
-        # # Test accuracy
-        # acc = accuracy_score(y_test, y_pred)
-        # print('acc = %.2f' % acc)
- # # Convert to np array for compatibility issues
- #        y_train = np.array(y_train).ravel()
- #        y_test = np.array(y_test).ravel()
 
 def load_eeg_pkl(filepath: str = None, return_all: bool = False):
     """Load EEG data from pkl file
@@ -202,6 +126,158 @@ def load_eeg_pkl(filepath: str = None, return_all: bool = False):
                trial_indexes, \
                dataset_tags, subject_tags, channels
     return ern_epochs_avg, non_ern_epochs_avg, dataset, labels, event_ids, channels
+
+
+def plot_eeg(data: pd.DataFrame, labels: tuple, fig_num: int = None,
+             columns=None, show: bool = None):
+    title, xlabel, ylabel = labels
+
+    X = pd.DataFrame(data.T, columns=columns)
+    plt.figure(1)
+    sns.lineplot(data=X, dashes=False)
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+
+    if show:
+        plt.show()
+
+
+def skf_split_train(n_splits: int, X: pd.DataFrame,
+                    y: pd.DataFrame) -> \
+        Tuple[list, np.ndarray, list, np.ndarray]:
+    """Returns a tuple that contains a list of X_train1 and y_train1 objects to
+    use to later train a classifier
+    """
+    skf = StratifiedKFold(n_splits=n_splits)
+    X_train_lst = []
+    X_test_lst = []
+    y_train_lst = []
+    y_test_lst = []
+
+    for i, (train_index, test_index) in enumerate(skf.split(X, y)):
+        X_train, X_test = np.array(X.iloc[train_index]), \
+                          np.array(X.iloc[test_index])
+        y_train, y_test = np.array(y.iloc[train_index]).ravel(), np.array(
+            y.iloc[test_index]).ravel()
+
+        # Get positive and negative occurrences
+        pos = np.where(y_train == 1)[0]
+        neg = np.where(y_train == 0)[0]
+
+        # Get X values that have ERN and do not have ERN
+        Xpos = X_train[pos, :]
+        Xneg = X_train[neg, :]
+
+        np.vstack((Xpos, Xneg))  # Stack all positive X's on the negative X's
+
+        ds_idx = Xneg.shape[0]  # Get index where we will begin downsampling
+
+        # Sample Xneg array len(XPos) many times
+        rand_ds_idx = np.random.randint(0, ds_idx, len(Xpos))
+
+        # Copy the array with the equal pos and equal downsampled neg values
+        X_train1 = np.vstack((Xpos, Xneg[rand_ds_idx]))
+
+        ypos = np.ones(Xpos.shape[0])
+        yneg = np.zeros(Xpos.shape[0])
+
+        y_train1 = np.concatenate((ypos, yneg), axis=0)
+
+        # Save the values used to train the classifier
+        X_train_lst.append(X_train1)
+        X_test_lst.append(X_test)
+        y_train_lst.append(y_train1)
+        y_test_lst.append(y_test)
+
+    # Pick a random X_test and y_test pair to use for testing
+    r = np.random.randint(n_splits)
+    X_test, y_test = X_test_lst[r], y_test_lst[r]
+
+    return X_train_lst, X_test, y_train_lst, y_test
+
+
+def svm_fit_clf(clf: svm.SVC, train_data: tuple) -> svm.SVC:
+    """Fits data to a passed classifier and returns the classifier. Assumes
+    first half of train_data are lists that contain X_train values, and latter
+    half are lists that contain y_train values. Both lists are of the same
+    length."""
+    X_train_lst = []
+    y_train_lst = []
+
+    # Unpack X_train and y_train values
+    x_y_split = len(train_data[0])
+    X_data = train_data[0]
+    y_data = train_data[1]
+
+    for i in np.arange(0, x_y_split):
+        X_train = X_data[i]
+        y_train = y_data[i]
+        X_train_lst.append(X_train)
+        y_train_lst.append(y_train)
+
+    # Fit the data
+    for j in np.arange(x_y_split):
+        clf.fit(X_train_lst[j], y_train_lst[j])
+
+    # Return the newly fit classifier
+    return clf
+
+
+def get_roc_data(clf: svm.SVC, X, y) -> Tuple[tuple, tuple]:
+    """Return two tuples with logistic ROC data and svm ROC data. Can be
+    plotted. Note: does not return the thresholds
+    """
+    # Use the classifier to predict given the X dataset
+    y_pred = clf.predict(X)
+    fpr, tpr, threshold = roc_curve(y, y_pred)
+    area_under_curve = auc(fpr, tpr)
+
+    y_pred_svm = clf.decision_function(X)
+    svm_fpr, svm_tpr, threshold = roc_curve(y, y_pred_svm)
+    svm_area_under_curve = auc(svm_fpr, svm_tpr)
+
+    logistic_roc = (fpr, tpr, area_under_curve)
+    svm_roc = (svm_fpr, svm_tpr, svm_area_under_curve)
+
+    return logistic_roc, svm_roc
+
+
+def best_n_splits(clf, fold_bounds: tuple, X, y) -> Tuple[svm.SVC, tuple,
+                                                          tuple]:
+    """Return the classifier that yields the highest tpr after iterating
+    through skf folds as many times as bounded by the parameters. Upper bound
+    is not exclusive. We test for SVM accuracy as this shows the efficiency
+    of the model.
+    """
+    lower_bound, upper_bound = fold_bounds
+    highest_svm_auc = 0.0
+    best_log_roc = None
+    best_svm_roc = None
+    best_n_folds = None
+    best_clf = None
+
+    for fold_idx in np.arange(lower_bound, upper_bound):
+        # Train and split the data
+        X_train_lst, X_test, y_train_lst, y_test = skf_split_train(fold_idx, X,
+                                                                   y)
+        clf = svm_fit_clf(clf, (X_train_lst, y_train_lst))
+
+        # Get ROC using the entire dataset
+        logistic_roc, svm_roc = get_roc_data(clf, X, y)
+        fpr, tpr, area_under_curve = logistic_roc
+        svm_fpr, svm_tpr, svm_area_under_curve = svm_roc
+
+        # Store classifier if it yields the highest svm auc
+        if svm_area_under_curve > highest_svm_auc:
+            highest_svm_auc = svm_area_under_curve
+            best_log_roc, best_svm_roc = logistic_roc, svm_roc
+            best_n_folds = fold_idx
+            best_clf = clf
+
+    print(f'Best number of folds for this trial is {best_n_folds} folds')
+    # Return the best classifier and its associated svm auc and logistic auc
+    return best_clf, best_svm_roc, best_log_roc
 
 
 if __name__ == "__main__":
